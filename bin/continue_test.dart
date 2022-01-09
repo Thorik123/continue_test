0afba975df086e10813ac2698fdb9962aa5455df
Thorik123 <tronikthorik07@gmail.com>
2022-01-09 17:27:52 +0700

    first commit

---
+++ bin/continue_test.dart
@@ -0,0 +1,11 @@
+void main() {
+  int angka = 0;
+  while (angka < 5) {
+    angka++;
+
+    if (angka == 3) {
+      continue;
+    }
+    print(angka);
+  }
+}
---
+++ test/continue_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:continue_test/continue_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
